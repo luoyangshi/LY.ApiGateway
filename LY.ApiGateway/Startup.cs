6edb3fba95d261a8f5d9c78545bf318ecd753909
曹武贝 <caowubei@outlook.com>
2019-11-14 17:23:16 +0800

    ocelot

--- LY.ApiGateway/Startup.cs
+++ LY.ApiGateway/Startup.cs
@@ -27,21 +27,23 @@ namespace LY.ApiGateway
         }
 
         public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddOcelot().AddConsul().AddPolly();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("gateway", new OpenApiInfo()
-                {
-                    Version = "v1",
-                    Title = "gateway api",
-                    Description = "อ๘นุ",
-                });
-                var basePath = AppContext.BaseDirectory;
-                var xmlPath = Path.Combine(basePath + "//xmls", "LY.ApiGateway.xml");
-                c.IncludeXmlComments(xmlPath);
+                //c.SwaggerDoc("gateway", new OpenApiInfo()
+                //{
+                //    Version = "v1",
+                //    Title = "gateway api",
+                //    Description = "อ๘นุ",
+                //});
+                //var basePath = AppContext.BaseDirectory;
+                //var xmlPath = Path.Combine(basePath + "//xmls", "LY.ApiGateway.xml");
+                //c.IncludeXmlComments(xmlPath);
+
                 c.OperationFilter<AddResponseHeadersFilter>();
                 c.OperationFilter<AppendAuthorizeToSummaryOperationFilter>();
                 c.OperationFilter<SecurityRequirementsOperationFilter>();
@@ -63,7 +65,6 @@ namespace LY.ApiGateway
             };
             services.AddAuthentication()
                 .AddIdentityServerAuthentication(authenticationProviderKey, options);
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -73,22 +74,20 @@ namespace LY.ApiGateway
             {
                 app.UseDeveloperExceptionPage();
             }
-           
-           
+
             app.UseRouting();
-            var apis=new List<string>(){"auth","order"};
+            var apis = new List<string>() { "auth", "order" };
             app.UseSwagger(c => { c.RouteTemplate = "{documentName}/swagger.json"; });
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/gateway/swagger.json", "gateway doc"); 
+                // c.SwaggerEndpoint("/gateway/swagger.json", "gateway doc");
                 apis.ForEach(m =>
                 {
-                    c.SwaggerEndpoint($"/{m}/swagger",$"{m} doc");
+                    c.SwaggerEndpoint($"/{m}/swagger", $"{m} doc");
                 });
             });
             app.UseOcelot().Wait();
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
-          
         }
     }
 }
